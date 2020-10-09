9d5fcbad948ff43ed915504dabdf1cf4646f4811
ahmdaeyz <ahmdaeyz@gmail.com>
2020-10-09 23:36:42 +0200

    Start the Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -29,11 +29,15 @@ public class InfoActivity extends AppCompatActivity {
         Uri storeLocAsUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, storeLocAsUri);
         mapIntent.setPackage("com.google.android.apps.maps");
-        if (mapIntent.resolveActivity(getPackageManager())!=null){
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(mapIntent);
         }
     }
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+
+    public void createPhoneIntent(View view) {
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        Uri dialUri = Uri.parse("tel:0123456789");
+        dialIntent.setData(dialUri);
+        startActivity(dialIntent);
+    }
 }
